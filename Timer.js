c2756f42906c44e434d07a9e304ffe82e6952966
nekk <demarcoguilhem@gmail.com>
2024-03-07 16:56:09 +0100

    start of interval circle

--- Timer.js
+++ Timer.js
@@ -1,16 +1,17 @@
 export default class Timer {
-    total = 0
     interval = 0
 
     constructor(total) {
         //console.log(total)
         this.total=total
+        this.max=total
     }
     //
     startTimer(callback) {
         console.log("test", this.total)
         this.interval = setInterval(()=>{          
             this.total = this.total - 1
+//offset= ((max-total)*dasharray)/max
             callback(this.total)
             console.log("total", typeof this.total, this.total)
             if (this.total <= 0) clearInterval(this.interval)   
--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import Timer from "./Timer.js"
 
 const timerInput = document.querySelector("input#time")
 const startTimeBtn = document.querySelector("button#start-btn")
+const stopTimeBtn = document.querySelector("button#stop-btn")
+
+let timer
 
 window.addEventListener("DOMContentLoaded", ()=>{
     startTimeBtn.addEventListener("click", startTimer)
+    stopTimeBtn.addEventListener("click", stopTimer)
 })
 
 function startTimer(){
     console.log("starting timer ", timerInput.value)
-    let timer = new Timer(timerInput.value)
+    timer = new Timer(timerInput.value)
     timer.startTimer((newTime)=>{
         timerInput.value = newTime
     })
+}
+function stopTimer(){
+    timer.stopTimer()
 }
